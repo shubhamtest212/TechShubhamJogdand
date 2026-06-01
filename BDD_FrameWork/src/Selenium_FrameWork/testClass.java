45619080dee4b09bdc66688c087ae72c5d3a9667
shubhamtest212 <shubhamjogdandtest@gmail.com>
2026-06-01 15:59:10 +0530

    New one class

--- BDD_FrameWork/src/Selenium_FrameWork/testClass.java
+++ BDD_FrameWork/src/Selenium_FrameWork/testClass.java
@@ -1,5 +1,13 @@
 package Selenium_FrameWork;
 
+import org.testng.annotations.Test;
+
 public class testClass {
+
+	@Test
+	public Class aMethod() {
+		System.out.println("A method");
+		return null;
+	}
 
 }
